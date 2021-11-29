2b0c2bd4fb792b7c764e69d979731f652c9b6625
AndreyRoa <arohau@exadel.com>
2021-11-29 11:34:02 +0300

    Lesson 2

--- Internship_c1/Program.cs
+++ Internship_c1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Internship_c1
 {
@@ -87,24 +88,95 @@ namespace Internship_c1
 
             //var name = Console.ReadLine();
 
-            Console.WriteLine("Choose an action:\n" +
-                "1. Say \"Hi\"\n" +
-                "2. Say \"Hello\"");
+            //Console.WriteLine("Choose an action:\n" +
+            //    "1. Say \"Hi\"\n" +
+            //    "2. Say \"Hello\"");
 
-            var choice = int.Parse(Console.ReadLine());
+            //var choice = int.Parse(Console.ReadLine());
 
-            switch (choice)
+            //switch (choice)
+            //{
+            //    case 1:
+            //        Hi();
+            //        break;
+            //    case 2:
+            //       Hello();
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+
+            //int num1 = 1;
+
+            //Dup1(num1);
+            //Console.WriteLine(num1); // 1
+
+            //Dup2(ref num1);
+            //Console.WriteLine(num1); // 2
+
+            //Sum(1, 2);
+            //Sum(1,2,3);
+
+            Console.WriteLine(Person.id);
+
+            var pers = new Person();
+
+            Console.WriteLine(Person.id);
+
+            var pers1 = new Person();
+
+            Console.WriteLine(Person.id);
+
+            pers.ShowPassport();
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female,
+            Unknown
+        }
+
+        public class Person
+        {
+            public static int id = 0;
+            public string FirstName 
             {
-                case 1:
-                    Hi();
-                    break;
-                case 2:
-                   Hello();
-                    break;
-                default:
-                    break;
+                get;
+                set;
+            }
+            public Gender Gender { get; set; }
+            public string LastName { get; set; }
+            public DateTimeOffset BirthDate;
+            public int Age {
+                get => DateTimeOffset.UtcNow.Year - BirthDate.Year;
             }
 
+            public Person()
+            {
+                ++id;
+                Gender = Gender.Unknown;
+                FirstName = "Vasya";
+                LastName = "Vasiliev";
+            }
+
+            public Person(string firstName, string lastName, int age)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+            }
+
+            public void ShowPassport()
+            {
+                Act();
+                Console.WriteLine($"FirstName: {FirstName}\nLast Name: {LastName}\nAge: {Age}, Gender: {Gender}");
+            }
+
+            private void Act()
+            {
+                Console.WriteLine("Put passport");
+            }
         }
 
         public static int ToInt(string number) => int.Parse(number);
@@ -129,6 +201,27 @@ namespace Internship_c1
             Monday,
             Friday,
             Wednesday
+        }
+        
+        public static void Dup2(ref int number)
+        {
+            number *= 2;
+        }
+
+        public static void Dup1(int number)
+        {
+            number *= 2;
+        }
+
+        public static void Sum(params int[] numbers)
+        {
+            var totalSum = 0;
+            foreach (var num in numbers)
+            {
+                totalSum += num;
+            }
+
+            Console.WriteLine(totalSum);
         }
     }
 }
